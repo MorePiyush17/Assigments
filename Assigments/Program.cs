fceac920059925939f8c3448b30819acfc228cd7
piyush.more <piyush.more@HP2356>
2025-06-17 07:40:20 -0700

    Add project files.

---
+++ Assigments/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Assigments
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+         //Factorial of Number 
+            Console.WriteLine("Enter a number: ");
+            int number = Convert.ToInt32(Console.ReadLine());
+            long factorial = 1;
+            for (int i = 1; i <= number; i++)
+            {
+                factorial *= i;
+            }
+            Console.WriteLine($"Factorial of {number} is : {factorial}");
+        }
+    }
+}
